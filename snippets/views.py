c5e17cabf8d6be684ff10c31ed574ef392bde47c
Yogesh Chauhan <yc.yogesh007@gmail.com>
2017-08-16 00:49:17 +0530

    added authentication custom permissions

--- snippets/views.py
+++ snippets/views.py
@@ -179,9 +179,11 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
 from rest_framework import permissions
+from snippets.permissions import IsOwnerOrReadOnly
 
 class SnippetList(generics.ListCreateAPIView):
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                            IsOwnerOrReadOnly)
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     def perform_create(self, serializer):
