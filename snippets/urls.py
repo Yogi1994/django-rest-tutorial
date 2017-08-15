957a72c48dd23d9ccfbce816d1f64fb44bc24f32
Yogesh Chauhan <yc.yogesh007@gmail.com>
2017-08-16 00:31:58 +0530

    added authentication, user serializer, connected snippet to user, ;;

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from snippets import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
+from django.conf.urls import include
 # urlpatterns = [
 #     url(r'^snippets/$', views.snippet_list),
 #     url(r'^snippets/(?P<pk>[0-9]+)$', views.snippet_detail),
@@ -9,6 +10,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     url(r'^snippets/$', views.SnippetList.as_view()),
     url(r'^snippets/(?P<pk>[0-9]+)$', views.SnippetDetail.as_view()),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^api-auth/', include('rest_framework.urls',
+    							namespace='rest_framework')),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -178,11 +178,28 @@ from django.shortcuts import render
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
+from rest_framework import permissions
 
 class SnippetList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
