e87d72050e098c6a95c8ef10d1795737e60f915c
Yogesh Chauhan <yc.yogesh007@gmail.com>
2017-08-17 01:34:20 +0530

    added highlighted links

--- drf/settings.py
+++ drf/settings.py
@@ -86,8 +86,11 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'PAGE_SIZE': 10
 }
 
 
