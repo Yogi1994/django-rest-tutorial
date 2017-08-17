dbebcc3967a09426decbeae2076cd88be53ae624
Yogesh Chauhan <yc.yogesh007@gmail.com>
2017-08-18 01:20:14 +0530

    added schema api

--- snippets/urls.py
+++ snippets/urls.py
@@ -57,10 +57,15 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
+from rest_framework.schemas import get_schema_view
 
+schema_view = get_schema_view(title='Pastebin API')
+
+    
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the login URLs for the browsable API.
 urlpatterns = [
+    url(r'^schema/$', schema_view),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
